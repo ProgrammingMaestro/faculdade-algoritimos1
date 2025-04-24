bd516f1ea126072463be771e03319213a1c69ea0
JuanCarlos <114189054+ProgrammingMaestro@users.noreply.github.com>
2025-04-23 23:43:16 -0400

    Delete exercicio_15.c

--- exercicio_15.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int a, b, c;
-    printf("Escolha Tres numeros: ");
-    scanf("%d %d %d", &a, &b, &c);
-    int soma = a + b + c;
-    printf("soma de a + b + c = %d\n", soma);
-    printf("media de a, b e c = %.2f\n", (soma/3.0));
-}
