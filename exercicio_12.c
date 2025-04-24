7551d666720b962339d03d58dcadff7fe6ff9c9e
JuanCarlos <114189054+ProgrammingMaestro@users.noreply.github.com>
2025-04-23 23:44:07 -0400

    Delete exercicio_12.c

--- exercicio_12.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	char letra;
-	char texto[6]; // define o limite de 5 letras +1 o '\0'
-	printf("Digite no maximo 5 letras: ");
-	scanf("%5s", texto); // le no mC!ximo 5 caracteres
-	printf("Voce digitou: %s\n", texto);
-}
