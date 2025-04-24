46d5201c6fba9cae655514054b19cd77e6fd08f4
JuanCarlos <114189054+ProgrammingMaestro@users.noreply.github.com>
2025-04-23 23:42:36 -0400

    Delete exercicio_06.c

--- exercicio_06.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-	int a, b, soma;
-	printf("Digite dois numeros inteiros para somar-los: ");
-	scanf("%d %d", &a, &b);
-	soma = a + b;
-	printf("O resultado eh: %d\n", soma);
-}
